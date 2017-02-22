653dbcf0c930f4ccdb79474bba4873e7c5222a78
Ingrivho <ingrid.vaage.hovland@gmail.com>
2017-02-22 14:00:18 +0100

    Sletta filer som ikkje skulle vere med på forrige commit :)

--- popupwindow.py
+++
@@ -1,5 +0,0 @@
-import ctypes  # An included library with Python install.
-
-def PopUpBox(title, text, style):
-    ctypes.windll.user32.MessageBoxW(0, text, title, style)
-PopUpBox('Your title', 'Your text', 1)
