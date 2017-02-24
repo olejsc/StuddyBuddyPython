75008144b44c39e31dd53f1fdad80854f0a9c69e
Ole Jakob Schjøth <olejsc@stud.ntnu.no>
2017-02-24 15:31:54 +0100

    Included popups and time-calculation of popups, mainapplication now initializes the popups at start. Known bug: Doesnt keep track of planned popups. and clicking okay on last popup crashes the program

--- MainApplication.py
+++ MainApplication.py
@@ -3,18 +3,11 @@ from tkinter import *
 from threading import Thread
 import pyglet
 import datetime
+from datetime import *
 import time
 import json
 
-# ----------------------------------------------------
-# GLOBALE VARIABLER
-# ----------------------------------------------------
 
-
-LARGE_FONT= ("Verdana", 12)
-name_dict = {"SubjectCode": "", "Freqency": "", "Name" : "", "FrequencyLength" : "", 'LastNotification': "", "LastNotificationSeen": "False"}
-notifications = get_all_notifications()
-options = {1: 5, 2: 2, 3: 3}
 
 
 # ----------------------------------------------------
@@ -23,7 +16,7 @@ options = {1: 5, 2: 2, 3: 3}
 
 def callback(e):
     input_string = str(e.get())
-    now = datetime.datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+    now = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
     name_dict["Name"] = input_string
     name_dict["Registered"]= now
     if get_notification_by_key_value('Name',input_string,notifications) == False:
@@ -31,7 +24,7 @@ def callback(e):
     e.delete(0, END) #Denne linjen sletter skriven etter ADD-knappen trykkes
     
 def quit_application (self):
-    now = datetime.datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+    now = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
     with open('log.txt', 'r+') as f:
         data = f.read()
         f.seek(0)
@@ -53,7 +46,6 @@ def get_all_notifications():
 # For example: ('hard topic','Name',list_of_notifications) will search all dictionaries
 # inside the list for with a key that have that name.
 def get_notification_by_key_value(target_value,key_value,notifications):
-    print(str(target_value) + " " + str(key_value))
     for notification in notifications:
         for key, value in notification.items():
             if ((str(key) == str(target_value))and ( str(value) == str(key_value))):
@@ -78,16 +70,16 @@ def check_if_notification_data_exist(target_key,target_value,notification):
 
 # TODO: INSERT COMMENT
 def set_notification_key_value(key, identifying_name, value):
-    notifications = get_all_notifications()
     target_value = 'Name'
     target_notification = get_notification_by_key_value(target_value,identifying_name,notifications)
-    old_value = target_notification[key]
     for notification in notifications:
-        if notification['Name']== identifying_name:
+        if notification[target_value]== identifying_name:
+            print(notification)
             notifications.remove(notification)
             break
         else:
             pass
+    target_notification[key] = value
     notifications.append(target_notification)
     temp = open('notifications.txt', 'w').close()
     with open('notifications.txt',"a") as f:
@@ -101,43 +93,58 @@ def set_notification_key_value(key, identifying_name, value):
 
 
 
-'''
+def next_popup_time(notification,opts, target_key):
+    not_dict = {}
+    frequency = notification['Frequency']
+    now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    last_notification = notification[target_key]
+    datetime_young = datetime.strptime(last_notification, "%y-%m-%d-%H-%M-%S")  # Formatvalg
+    datetime_now = datetime.strptime(now, "%y-%m-%d-%H-%M-%S")  # Formatvalg
+    time_delta = datetime_now - datetime_young
+    frequency_millisecond = (timedelta(milliseconds=int(frequency)).total_seconds())*1000
+    to_milli = time_delta.total_seconds()*1000
+    result = to_milli - frequency_millisecond
+    if result > 0 :
+        not_dict[result] = notification
+        if 'show_now' in opts.keys():
+            opts['show_now'][result] = notification
+            print(opts)
+            return opts
+        else:
+            opts['show_now']= {result:notification}
+            print(opts)
+        return opts
+    
+    elif result <= 0:
+        not_dict[result] = notification
+        if 'show_later' in opts.keys():
+            opts['show_later'][result] = notification
+        else:
+            opts['show_later']= {result:notification}
+        return opts
+
+
 def get_time_of_notification(notifications):
-    opts = { Opt1: {'Time':{notification1},'Time2':{notification2}},Opt2:{'Time1':{Not1},'Time2':{not2}},Opt3:{'Time1':{Not1},'Time2':{not2}}}
+    opts = {}
     for notification in notifications:
-        
-        registered = notification['Registered']
-        if notification['LastNotificationSeen'] == 'True' and notification['LastNotification'] != "":
-            # Calculates next popup, Nåtid minus LastNotification
-            # return a dict with { time_to_popup : {notification}
-             popup_return_time_dict = calculate_when_next_popup_will_be(notification,options,1)
-             insert_popup_into_correct_dict_position_in_opts
-
-             
-        elif notification['LastNotificationSeen'] == 'False' and notification['LastNotification'] != "":
-            # Calculates next popup, Now minus LastNotification
-            # If the time is there to show popup, return true or something :p If not, return a dict with { time_to_popup : {notification}} 
-            if calculate_when_next_popup will be(notification,options,2) == True:
-                 show_popup(notification)
-                 
-            # If not, return a dict with { time_to_popup : {notification}} 
-            else:
-                dict_with_calculated_time = calculate_when_next_popup will be(notification,options,2)
-
-                
+        if ((notification['LastNotificationSeen'] == 'True' or notification['LastNotificationSeen'] == 'False')
+            and notification['LastNotification'] != ""):
+            target_key = 'LastNotification'
+            opts = next_popup_time(notification,opts, target_key)
         elif notification['LastNotification'] == "":
-            # Calculates next popup, Now minus Registered
-            # return a dict with { time_to_popup : {notification}} 
-            calculate_when_next_popup will be(notification,options,)
-            insert_popup_into_correct_dict_position_in_opts
-'''
+            target_key = 'Registered'
+            opts = next_popup_time(notification,opts, target_key)
+        else:
+            pass
+    print("FUCKINGS DONE")
+    return opts
             
-def popupmsg(notification, ):
+def popupmsg(self,subject):
     popup = tk.Tk()
     popup.wm_title("!")
-    label = tk.Label(popup, text=msg, font=LARGE_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    label1 = Label(popup, text="Time to read!" + "\n" + "The subject is: " + str(subject['Registered'])+"---" +str(subject['LastNotification']), font=LARGE_FONT)
+    label1.pack()
+    B1 = Button(popup, text="Okay", command = self.destroy)
     B1.pack()
     popup.mainloop()
         
@@ -224,34 +231,71 @@ class MainApplication(tk.Tk):
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Exit", command=lambda: quit_application(self))
         menubar.add_cascade(label="File", menu=filemenu)
-
         tk.Tk.config(self, menu=menubar)
-
+        value2 = 'asdsadsafwggs'
         self.frames = {}
+        
+        for F in (Add_notification,):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
         frame = Add_notification(container, self)
-
         self.frames[Add_notification] = frame
-
         frame.grid(row=0, column=0, sticky="nsew")
-        self.root = tk.Tk()
-        self.label = tk.Label(text="")
-        self.label.pack()
+        opts = {}
+        opts = get_time_of_notification(notifications)
+        for key, value in opts.items():
+            if key == 'show_now':
+                for key2, value2 in value.items():
+                    print("HADE" + str(key2))
+                    notification_window = popup(tk.Tk(), value2, LARGE_FONT)
+                    key_now = 'LastNotification'
+                    now2 = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+                    set_notification_key_value(key_now, value2['Name'], now2)
+            if key == 'show_later':
+                for key3, value3 in value.items():
+                    if value3['LastNotificationSeen'] == 'False':
+                        key_later = 'LastNotification'
+                        now3 = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+                        popup_after(self,value,key3)
+                        set_notification_key_value(key_later, value3['Name'], now3)
+                    else:
+                        pass
 
+        if notification_window:
+            playsound(self)
+    
         self.show_frame(Add_notification)
-        self.update_clock()
+        
+        '''self.update_clock()'''
+        '''self.update_notifications'''
 
-    def update_clock(self):
+    '''def update_clock(self):
         now = time.strftime("%H:%M:%S")
         self.label.configure(text=now)
         self.root.after(1000, self.update_clock)
+        '''
+
 
     def show_frame(self, cont):
 
         frame = self.frames[cont]
         frame.tkraise()
 
+    def real_playsound(self):
+        sound = pyglet.media.load('alarm2wav.wav')
+        sound.play()
+        pyglet.app.run()
 
+    def playsound(self):
+        global player_thread
+        player_thread = Thread(target=self.real_playsound)
+        player_thread.start()
+
+    def popup_after(self,value,frequency):
+        print("HEI" + str(key3))
+        self.root.after(frequency,notification_window = popup(tk.Tk(), value, LARGE_FONT))
 
 # ----------------------------------------------------
 # GLOBALE VARIABLER
@@ -268,7 +312,7 @@ class Add_notification(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         #Dette er Meldingen som vises
-        label = tk.Label(self, text=("""Add Serious hard subjects to learn here"""), font=LARGE_FONT, bg = "WHITE")
+        label = tk.Label(self, text=("""ADD SERIOUS SHIT HERE"""), font=LARGE_FONT, bg = "WHITE")
         label.pack(pady=10,padx=10)
 
         #Dette er feltet vi skriver inn i:
@@ -279,11 +323,33 @@ class Add_notification(tk.Frame):
         b = Button(self, text="ADD", width=5, pady = 10, padx = 5, command=lambda: callback(e), font="Arial", )
         b.pack()
 
-
         #Dette var bare pynt
         #separator = Frame(height=10, bd=1, relief=SUNKEN)
         #separator.pack(fill=X, padx=5, pady=5)
+        
+class popup(tk.Frame):
+    def __init__(self,parent, subject, font):
+        tk.Frame.__init__(self,parent)
+        self.parent = parent
+        self.subject = subject # hvilket fag/tema som skal leses på, må hentes fra det som er lagret i fil
+        self.font=font
+        label2 = Label(parent, text="Time to read!" + "\n" + "Registered date is: " + "\n" + 
+                       str(subject['Registered'])+"\n" +"LastNotification is:" +str(subject['LastNotification'])
+                    +"\n" +"LastNotificationSeen is: " +str(subject['LastNotificationSeen']), font=self.font)
+        label2.pack()
 
+        #self.subjectLabel = Label(master, text="" + subject, font=self.font, color=)
+        self.close_button = Button(parent, text="Got it!",
+                command=lambda: quit_and_store(parent, subject['Name'], 'True'))
+        self.close_button.pack()
+
+
+
+
+def quit_and_store(parent, identifying_name, value):
+    key = 'LastNotificationSeen'
+    set_notification_key_value(key, identifying_name, value)
+    parent.destroy()
 
 
 def main(): 
