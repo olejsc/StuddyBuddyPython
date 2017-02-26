419f5b7c76e2f436ca06cf33df2ab7287ecd3eab
Ole Jakob Schjøth <olejsc@stud.ntnu.no>
2017-02-26 23:37:00 +0100

    Stable edition, popups which are supposed to show later are disabled

--- MainApplication.py
+++ MainApplication.py
@@ -4,7 +4,6 @@ from threading import Thread
 import pyglet
 import datetime
 from datetime import *
-import time
 import json
 
 
@@ -19,6 +18,7 @@ def callback(e):
     now = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
     name_dict["Name"] = input_string
     name_dict["Registered"]= now
+    name_dict["Frequency"] = "300000"
     if get_notification_by_key_value('Name',input_string,notifications) == False:
         add_notification_to_file(name_dict)
     e.delete(0, END) #Denne linjen sletter skriven etter ADD-knappen trykkes
@@ -74,7 +74,6 @@ def set_notification_key_value(key, identifying_name, value):
     target_notification = get_notification_by_key_value(target_value,identifying_name,notifications)
     for notification in notifications:
         if notification[target_value]== identifying_name:
-            print(notification)
             notifications.remove(notification)
             break
         else:
@@ -108,18 +107,19 @@ def next_popup_time(notification,opts, target_key):
         not_dict[result] = notification
         if 'show_now' in opts.keys():
             opts['show_now'][result] = notification
-            print(opts)
             return opts
         else:
             opts['show_now']= {result:notification}
-            print(opts)
         return opts
     
     elif result <= 0:
         not_dict[result] = notification
         if 'show_later' in opts.keys():
+            print("Existing keys in show_later, adding:")
+            print(result)
             opts['show_later'][result] = notification
         else:
+            print(result)
             opts['show_later']= {result:notification}
         return opts
 
@@ -136,34 +136,9 @@ def get_time_of_notification(notifications):
             opts = next_popup_time(notification,opts, target_key)
         else:
             pass
-    print("FUCKINGS DONE")
+    print("Ferdig med tidsberegninger for hver notifikasjon...")
     return opts
             
-def popupmsg(self,subject):
-    popup = tk.Tk()
-    popup.wm_title("!")
-    label1 = Label(popup, text="Time to read!" + "\n" + "The subject is: " + str(subject['Registered'])+"---" +str(subject['LastNotification']), font=LARGE_FONT)
-    label1.pack()
-    B1 = Button(popup, text="Okay", command = self.destroy)
-    B1.pack()
-    popup.mainloop()
-        
-
-
-
-'''def __init__(self, master, subject,):
-    self.master = master
-    master.title("Notification")
-
-    self.subject = subject # hvilket fag/tema som skal leses på, må hentes fra det som er lagret i fil
-    self.font=tkinter.font.Font(family='Helvetica', size=26, weight='bold')
-    self.label = Label(master, text="Time to read!" + "\n" + "The subject is: " + subject, font=self.font)
-    self.label.pack()
-
-    #self.subjectLabel = Label(master, text="" + subject, font=self.font, color=)
-    self.close_button = Button(master, text="Got it!", command=master.destroy)
-    self.close_button.pack()
-'''
 
 def real_playsound(self):
     sound = pyglet.media.load('alarm2wav.wav')
@@ -182,35 +157,6 @@ def shut_down(self):
     self.quit()
 
 
-def time_when_notified(notification,options,requested_logic):
-    for key, value in notification.items():  # Går gjennom elementene i dictionary
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Finner ut tiden nå.
-            solve = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")  # Finner ut og legger til når personen vil bli varslet
-            solve += timedelta(seconds=value)  # -''-
-            notification_time = solve.strftime("%Y-%m-%d %H:%M:%S")  # Variabel for senere bruk
-            return notification_time
-
-
-#  Sender varsel når nåtid er lik varseltid.
-
-
-def check_vol2(self):
-    last_notification = time_when_notified(variable.get())  # Lager variabel med angitt varseltid. Tid i framtiden.
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    datetime_young = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")  # Formatvalg
-    datetime_older = datetime.strptime(last_notification, "%Y-%m-%d %H:%M:%S")  # Formatvalg
-    time_since_last = datetime_older - datetime_young
-    difference = (time_since_last.total_seconds() / 3600)
-    while True:
-        if difference == 0:
-            break
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        datetime_young = datetime.strptime(now, "%Y-%m-%d %H:%M:%S")  # Formatvalg
-        time_since_last = datetime_older - datetime_young
-        difference = (time_since_last.total_seconds()/3600)
-    print("Du skal få varsel nå")  # Kjør notifikasjonskode
-
-
 # ----------------------------------------------------
 # ----------------------------------------------------
 # FUNGERENDE PROGRAM UNDER:
@@ -221,7 +167,8 @@ class MainApplication(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
-
+        self.geometry("400x400")
+        print('Initialiserer MainApplication...')
         container.pack(side="top", fill="both", expand = True)
 
         container.grid_rowconfigure(0, weight=1)
@@ -232,52 +179,43 @@ class MainApplication(tk.Tk):
         filemenu.add_command(label="Exit", command=lambda: quit_application(self))
         menubar.add_cascade(label="File", menu=filemenu)
         tk.Tk.config(self, menu=menubar)
-        value2 = 'asdsadsafwggs'
         self.frames = {}
         
         for F in (Add_notification,):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-
+        print('Initialiserer Add_notification screen...')
         frame = Add_notification(container, self)
         self.frames[Add_notification] = frame
         frame.grid(row=0, column=0, sticky="nsew")
-        opts = {}
         opts = get_time_of_notification(notifications)
+        notification_window = False
         for key, value in opts.items():
             if key == 'show_now':
                 for key2, value2 in value.items():
-                    print("HADE" + str(key2))
-                    notification_window = popup(tk.Tk(), value2, LARGE_FONT)
-                    key_now = 'LastNotification'
+                    noti = 'now'
                     now2 = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
+                    notification_window = True
+                    key_now = 'LastNotification'
                     set_notification_key_value(key_now, value2['Name'], now2)
-            if key == 'show_later':
+                    topLevel(key2,value2,noti)
+                    print('Showing popups now...')
+            elif key == 'show_later':
                 for key3, value3 in value.items():
-                    if value3['LastNotificationSeen'] == 'False':
+                    if value3['LastNotificationSeen'] == 'True':
+                        noti = 'delayed'
+                        self.after(key_ms,topLevel(key3,value3,noti))
+                        notification_window = True
                         key_later = 'LastNotification'
                         now3 = datetime.now().strftime('%y-%m-%d-%H-%M-%S')
-                        popup_after(self,value,key3)
                         set_notification_key_value(key_later, value3['Name'], now3)
                     else:
                         pass
-
-        if notification_window:
+        if notification_window == True:
             playsound(self)
     
         self.show_frame(Add_notification)
-        
-        '''self.update_clock()'''
-        '''self.update_notifications'''
-
-    '''def update_clock(self):
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(1000, self.update_clock)
-        '''
-
-
     def show_frame(self, cont):
 
         frame = self.frames[cont]
@@ -293,9 +231,7 @@ class MainApplication(tk.Tk):
         player_thread = Thread(target=self.real_playsound)
         player_thread.start()
 
-    def popup_after(self,value,frequency):
-        print("HEI" + str(key3))
-        self.root.after(frequency,notification_window = popup(tk.Tk(), value, LARGE_FONT))
+
 
 # ----------------------------------------------------
 # GLOBALE VARIABLER
@@ -312,9 +248,8 @@ class Add_notification(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         #Dette er Meldingen som vises
-        label = tk.Label(self, text=("""ADD SERIOUS SHIT HERE"""), font=LARGE_FONT, bg = "WHITE")
+        label = tk.Label(self, text=("""Add subject here"""), font=LARGE_FONT, bg = "WHITE")
         label.pack(pady=10,padx=10)
-
         #Dette er feltet vi skriver inn i:
         e = Entry(self)
         e.pack()
@@ -326,30 +261,40 @@ class Add_notification(tk.Frame):
         #Dette var bare pynt
         #separator = Frame(height=10, bd=1, relief=SUNKEN)
         #separator.pack(fill=X, padx=5, pady=5)
-        
-class popup(tk.Frame):
-    def __init__(self,parent, subject, font):
-        tk.Frame.__init__(self,parent)
-        self.parent = parent
-        self.subject = subject # hvilket fag/tema som skal leses på, må hentes fra det som er lagret i fil
-        self.font=font
-        label2 = Label(parent, text="Time to read!" + "\n" + "Registered date is: " + "\n" + 
-                       str(subject['Registered'])+"\n" +"LastNotification is:" +str(subject['LastNotification'])
-                    +"\n" +"LastNotificationSeen is: " +str(subject['LastNotificationSeen']), font=self.font)
-        label2.pack()
-
-        #self.subjectLabel = Label(master, text="" + subject, font=self.font, color=)
-        self.close_button = Button(parent, text="Got it!",
-                command=lambda: quit_and_store(parent, subject['Name'], 'True'))
-        self.close_button.pack()
 
 
+def topLevel(frequency,subject, type_of_not):
+    top=Toplevel()
+    top.attributes("-topmost", True)
+    top.title("Notification!")
+    label1 = Label(top, text="Time to read!" + "\n" + str(subject['Name']), font=('Times', 14))
+    label1.grid(row=0,column = 1, sticky = N)
+    label2 = Label(top, text="Fag:" + "\n" + str(subject['SubjectCode']), font=('Times', 10))
+    label2.grid(row=1,column = 1, sticky = N)
+    close_button = Button(top, text="Got it!", command=lambda: quit_and_store(top, subject['Name'], 'True'))
+    close_button.grid(row = 2,column = 1, sticky = N)
+    '''label1 = Label(top, text="Registered date is: " + "\n" + str(subject['Registered']), font=('Times', 12))
+    label1.grid(row=0,column = 0, sticky = N)
+    label2 = Label(top, text="LastNotification is:" + "\n" +str(subject['LastNotification']), font=('Times', 12))
+    label2.grid(row=0,column = 1, sticky = W)
+    label3 = Label(top, text="LastNotificationSeen is:" + "\n" +str(subject['LastNotificationSeen']), font=('Times', 12))
+    label3.grid(row=0,column = 2, sticky = W)
+    label4 = Label(top, text="Result after time calculation " + "\n" +str(int(frequency)), font=('Times', 12))
+    label4.grid(row=1,column = 0, sticky = W)
+    label5 = Label(top, text="Notification type: " + "\n" + type_of_not, font=('Times', 12))
+    label5.grid(row=1,column = 2, sticky = W)
+    label6 = Label(top, text="Frequency in ms: " + "\n" + str(int(subject['Frequency'])), font=('Times', 12))
+    label6.grid(row=1,column = 1, sticky = W)
+    label7 = Label(top, text="Name is: " + "\n" + str(subject['Name']), font=('Times', 12))
+    label7.grid(row=2,column = 0, sticky = W)
+    close_button = Button(top, text="Got it!", command=lambda: quit_and_store(top, subject['Name'], 'True'))
+    close_button.grid(row = 2,column = 1, sticky = N)'''
 
 
-def quit_and_store(parent, identifying_name, value):
+def quit_and_store(self, identifying_name, value):
     key = 'LastNotificationSeen'
     set_notification_key_value(key, identifying_name, value)
-    parent.destroy()
+    self.destroy()
 
 
 def main(): 
